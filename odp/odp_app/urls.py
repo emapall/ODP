aff79534e73ee95f5aab87e9d255e2d84c94a484
Recursing <buonanno.lorenzo@gmail.com>
2019-10-19 17:50:11 +0200

    update urls syntax

--- odp/odp_app/urls.py
+++ odp/odp_app/urls.py
@@ -1,43 +1,36 @@
 # -*- coding: utf-8 -*-
 
 # from django.conf.urls.defaults import *
-from django.conf.urls import *
-from django.views.generic import RedirectView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from lider.odp import views as core_views
+from django.urls import path
+from odp_app import views as core_views
 
-urlpatterns = patterns(
-    "",
-    (r"^search/s_results/$", "lider.odp.views.s_results"),
-    (r"^search/new_s_results/$", "lider.odp.views.new_s_results"),
-    (r"^search/s_details/$", "lider.odp.views.s_details"),
-    (r"^search/i_results/$", "lider.odp.views.i_results"),
-    (r"^search/i_details/$", "lider.odp.views.i_details"),
-    (r"^search/d_results/$", "lider.odp.views.d_results"),
-    # 	(r'^search/?$', 'django.views.generic.simple.direct_to_template', {'template':'odp/search.html'}),
-    (r"^search/?$", "lider.odp.views.new_search"),
-    (r"^/?$", "lider.odp.views.new_search"),
-    (r"^search_ns/?$", "lider.odp.views.search_noscript"),
-    (r"^ajax/assicurazioni.json$", "lider.odp.views.json_assicurazioni"),
-    (r"^ajax/professioni.json$", "lider.odp.views.json_professioni"),
-    (r"^ajax/lesioni.json$", "lider.odp.views.json_lesioni"),
-    (r"^ajax/postumi.json$", "lider.odp.views.json_postumi"),
-    (r"^ajax/profili_rilevanti.json$", "lider.odp.views.json_profili_rilevanti"),
-    (r"^ajax/profilo.json$", "lider.odp.ajax.update_profilo"),
-    (
-        r"^login/?$",
-        "django.contrib.auth.views.login",
-        {"template_name": "odp/login.html"},
-    ),
-    (r"^logout/?$", "lider.odp.views.logout_view"),
-    (r"^register/?$", "lider.odp.views.singup"),
-    (r"^admin/?", RedirectView.as_view(url="/database/admin/")),
-    (r"^new_search/?$", "lider.odp.views.new_search"),
-    (r"^account_activation_sent/$", "lider.odp.views.account_activation_sent"),
-) + [
+urlpatterns = [
+    path("search/s_results/", core_views.s_results),
+    path("search/new_s_results/", core_views.new_s_results),
+    path("search/s_details/", core_views.s_details),
+    path("search/i_results/", core_views.i_results),
+    path("search/i_details/", core_views.i_details),
+    path("search/d_results/", core_views.d_results),
+    # (r'^search/?$', 'django.views.generic.simple.direct_to_template', {'template':'odp/search.html'}),
+    path("search/", core_views.new_search),
+    path("/", core_views.new_search),
+    # path("search_ns/", core_views.search_noscript),
+    path("ajax/assicurazioni.json", core_views.json_assicurazioni),
+    path("ajax/professioni.json", core_views.json_professioni),
+    path("ajax/lesioni.json", core_views.json_lesioni),
+    path("ajax/postumi.json", core_views.json_postumi),
+    path("ajax/profili_rilevanti.json", core_views.json_profili_rilevanti),
+    # path("ajax/profilo.json", "lider.odp.ajax.update_profilo"),
+    # path("login/", "django.contrib.auth.views.login", {"template_name": "odp/login.html"}), # TODO
+    path("logout/", core_views.logout_view),
+    # path("register/", core_views.singup), # TODO
+    # path("admin/", RedirectView.as_view(url="/database/admin/")), # TODO
+    path("account_activation_sent/", core_views.account_activation_sent),
+]
+""" + [
     url(
-        r"^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
+        "activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",
         core_views.activate,
         name="activate",
     )
-]
+] """
