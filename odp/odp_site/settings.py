7aae1be9e8859ae5f7171759d1bb5d8ed4180ecd
ema <ema.pallaoro@gmail.com>
2020-03-16 21:34:02 +0100

    boh

--- odp/odp_site/settings.py
+++ odp/odp_site/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "odp_app",
+    "odp_app", #why not odp_app.apps.OdpAppConfig?
 ]
 
 MIDDLEWARE = [
