cd5e4e4563794d96d5769b499f880eed3e3834d7
ema <ema.pallaoro@gmail.com>
2020-04-14 22:11:15 +0200

    added db

--- odp/odp_site/settings.py
+++ odp/odp_site/settings.py
@@ -84,9 +84,11 @@ WSGI_APPLICATION = "odp_site.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db.conf'),
+        },
     }
 }
 
