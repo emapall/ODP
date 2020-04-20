a8c093b473f67b60099e27d0ad3dc7d99d23c725
ema <ema.pallaoro@gmail.com>
2020-04-20 11:48:22 +0200

    per devel-db-speriamo

--- odp/odp_site/settings.py
+++ odp/odp_site/settings.py
@@ -84,10 +84,8 @@ WSGI_APPLICATION = "odp_site.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'db.conf'),
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
