8fba0f355586e011209c9d49033865b7c70a7669
ema <ema.pallaoro@gmail.com>
2021-01-24 12:20:22 +0100

    changed file serving to account for CTU & future files

--- odp/odp_app/views/files_serving.py
+++ odp/odp_app/views/files_serving.py
@@ -5,26 +5,34 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import PermissionDenied
 from django.conf import settings
 
-from odp_app.models import Sentenza
+from odp_app.models import Sentenza, Ctu#TODO Ctu
 
 from sendfile import sendfile
 
 
 @login_required
-def get_file(request,sent_id,field_name): #filename may be a filename or a path, but i have not yet implemented the "path" part in the upload phase
+def get_file(request, obj_name, obj_id, field_name): #filename may be a filename or a path, but i have not yet implemented the "path" part in the upload phase
     #check whicherver permission
-    sent = get_object_or_404(Sentenza,pk=sent_id)
+    # In case it's a Sentenza object
+    if obj_name=="s":
+        obj = get_object_or_404(Sentenza,pk=obj_id)
 
-    # security check
-    if sent.forza_esclusione and (not request.user.is_staff):
-        raise PermissionDenied
-    if not request.user.is_staff:
-        for infortunato in sent.infortunati.all():
-            if not infortunato.pubblicabile:
+        # security check
+        if obj.forza_esclusione and (not request.user.is_staff):
+            raise PermissionDenied
+        if not request.user.is_staff:
+            for infortunato in obj.infortunati.all():
+                if not infortunato.pubblicabile:
+                    raise PermissionDenied
+    # in case it's a Ctu object
+    if obj_name == "c":
+        obj = get_object_or_404(Ctu, pk=obj_id) # (SECURITY?? INFORTUNATO PUBBLICABILE? 404 ) 
+        if not request.user.is_staff:
+            if not obj.infortunato.pubblicabile:
                 raise PermissionDenied
-
+    
     # can access document
-    doc = getattr(sent,field_name)
+    doc = getattr(obj,field_name)
     filename = doc.name
     real_path = os.path.join(
                 settings.MEDIA_ROOT, 
