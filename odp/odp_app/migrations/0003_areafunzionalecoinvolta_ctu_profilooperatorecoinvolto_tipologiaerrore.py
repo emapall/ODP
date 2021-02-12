ca8f7f4949ef4a92897948a9a914cb783e72021b
ema <ema.pallaoro@gmail.com>
2021-02-12 10:55:36 +0100

    fix migrations in order to catch up with CTU - issues on remote server

--- odp/odp_app/migrations/0003_areafunzionalecoinvolta_ctu_profilooperatorecoinvolto_tipologiaerrore.py
+++ odp/odp_app/migrations/0003_areafunzionalecoinvolta_ctu_profilooperatorecoinvolto_tipologiaerrore.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('odp_app', '0002_auto_20200604_1845'),
+        ('odp_app', '0001_initial'),
     ]
 
     operations = [
