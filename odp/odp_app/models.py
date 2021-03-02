dfcf7e144ba3a8f8cb3ffb9206ba952d78147dcc
ema <ema.pallaoro@gmail.com>
2021-03-02 16:17:10 +0100

    correzione bug nei modello CTU - c'era ancora infortunato

--- odp/odp_app/models.py
+++ odp/odp_app/models.py
@@ -1807,7 +1807,7 @@ class Ctu(models.Model):
         )
 
     def __str__(self):
-        return ("%s-%s" % (str(self.infortunato),str(self.infortunato.sentenza)) )
+        return ("%s" % str(self.sentenza))
 
     class Meta:
         verbose_name = u"Scheda CTU"
